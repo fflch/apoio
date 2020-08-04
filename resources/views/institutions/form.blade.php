cabed6e549bff3f49e56a3333cf0a705e3dda204
Ricardo Fontoura <ricardfo@usp.br>
2020-08-04 20:03:49 -0300

    corrigindo o form do model institution
        - adicionado as classes do bootstrap.

--- resources/views/institutions/form.blade.php
+++ resources/views/institutions/form.blade.php
@@ -1,27 +1,35 @@
-<div>
-  <label class="">Sigla</label>
-  <div>
-    <input type="text" class="" name="sigla" value="{{ old('sigla') }}">
+<div class="form-group row">
+  <label for="sigla" class="col-sm-2 col-form-label">Sigla</label>
+  <div class="col-sm-3">
+    <input type="text" class="form-control" name="sigla" id="sigla"
+    value="{{ old('sigla') }}">
   </div>
 </div>
-<div>
-  <label>Nome</label>
-  <div>
-    <input type="text" class="" name="nome" value="{{ old('nome') }}">
+
+<div class="form-group row">
+  <label for="nome" class="col-sm-2 col-form-label">Nome</label>
+  <div class="col-sm-10">
+    <input type="text" class="form-control" name="nome" id="nome"
+    value="{{ old('nome') }}">
   </div>
 </div>
-<div>
-  <label>Unidade</label>
-  <div>
-    <input type="text" class="" name="unidade" value="{{ old('unidade') }}">
+
+<div class="form-group row">
+  <label for="unidade" class="col-sm-2 col-form-label">Unidade</label>
+  <div class="col-sm-3">
+    <input type="text" class="form-control" name="unidade" id="unidade"
+    value="{{ old('unidade') }}">
   </div>
 </div>
-<div>
-  <label>Local</label>
-  <div>
-    <input type="text" class="" name="local" value="{{ old('local') }}">
+
+<div class="form-group row">
+  <label for="local" class="col-sm-2 col-form-label">Local</label>
+  <div class="col-sm-3">
+    <input type="text" class="form-control" name="local" id="local"
+    value="{{ old('local') }}">
   </div>
 </div>
+
 <div class="">
-  <button type="submit" class="">Enviar</button>
+  <button type="submit" class="btn btn-primary">Enviar</button>
 </div>
