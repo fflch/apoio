0c9671a76aee0bbcf39ce2e49bd66d2c4fa5df8d
Ricardo Fontoura <ricardfo@usp.br>
2020-08-17 17:01:53 -0300

    adicionado o controller e rotas para Departamentos
        - adicionado o controller DepartamentController;
        - adicionado a rota resource para Departamento.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::any('/institutions/search', 'InstitutionController@search')
     ->name('institutions.search');
 Route::resource('/institutions', 'InstitutionController');
 
+Route::resource('/departaments', 'DepartamentController');
+
 Route::get('/', function () {
     return view('welcome');
 });
