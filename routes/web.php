0a5cf94eca7098be34576863fbabbfa72ea17c96
Ricardo Fontoura <ricardfo@usp.br>
2020-09-25 14:26:43 -0300

    corrigindo as definições de rotas
        Agora está no padrão do laravel 8

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,36 @@
 <?php
 
+use App\Http\Controllers\InstitutionController;
+use App\Http\Controllers\DepartamentController;
+use App\Http\Controllers\DesignationController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ContactTypeController;
+use App\Http\Controllers\AreaController;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/institutions/search', 'InstitutionController@search')
+Route::any('/institutions/search', [InstitutionController::class,'search'])
     ->name('institutions.search');
-Route::resource('/institutions', 'InstitutionController');
+Route::resource('/institutions', InstitutionController::class);
 
-Route::any('/departaments/search', 'DepartamentController@search')
+Route::any('/departaments/search', [DepartamentController::class,'search'])
     ->name('departaments.search');
-Route::resource('/departaments', 'DepartamentController');
+Route::resource('/departaments', DepartamentController::class);
 
-Route::any('/designations/search', 'DesignationController@search')
+Route::any('/designations/search', [DesignationController::class,'search'])
     ->name('designations.search');
-Route::resource('/designations', 'DesignationController');
+Route::resource('/designations', DesignationController::class);
 
-Route::any('/roles/search', 'RoleController@search')
+Route::any('/roles/search', [RoleController::class,'search'])
     ->name('roles.search');
-Route::resource('/roles', 'RoleController');
+Route::resource('/roles', RoleController::class);
 
-Route::any('/contact_types/search', 'ContactTypeController@search')
+Route::any('/contact_types/search', [ContactTypeController::class,'search'])
     ->name('contact_types.search');
-Route::resource('/contact_types', 'ContactTypeController');
+Route::resource('/contact_types', ContactTypeController::class);
 
-Route::any('/areas/search', 'AreaController@search')
+Route::any('/areas/search', [AreaController::class,'search'])
     ->name('areas.search');
-Route::resource('/areas', 'AreaController');
+Route::resource('/areas', AreaController::class);
 
 Route::get('/', function () {
     return view('app');
